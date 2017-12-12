b18e762c38a73f2e229be5f1d773bcbfb65d113f
VirginiaCardoso <virgicardoso@gmail.com>
2017-12-12 09:22:48 -0300

    commit inicial

---
+++ llamadas.php
@@ -0,0 +1,31 @@
+<!doctype html>
+			<html>
+				<head>
+					<link href='../../imgs/favicon.png' rel='icon' type='image/png'/>
+					<meta name="viewport" content="width=device-width,initial-scale=1">
+					<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+					<link rel="icon" href="imgs/favicon.icon" type="image/x-icon" />
+					<link rel="stylesheet" type="text/css" href="css/llamadas_turnos.css">
+					<title>Hospital Municipal de Agudos | Llamada turnos</title>
+				</head>
+				
+				<body>
+					<div id="mostrar_ultimo" class="contenedor">
+						<div id="div_po_nompac" class="info_ultimo">
+							CARDOSO 
+							<br>
+							VIRGINIA 
+						
+						</div>
+						<div id="div_po_cons" class="info_ultimo">
+							CONSULTORIO 2
+						</div>
+						<div id="div_po_espec" class="info_ultimo">
+							CLÍNICA MÉDICA
+						</div>
+					</div>
+					<div id="mini_pool" class="contenedor">
+						ultimos 5 turnos llamados
+					</div>
+				</body>
+			</html>
